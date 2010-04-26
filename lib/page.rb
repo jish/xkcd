d142cbe973feecb24064324d767d01a1f797c000
Josh Lubaway <josh.lubaway@gmail.com>
2010-04-25 21:00:45 -0700

    Adding a page object.

---
+++ lib/page.rb
@@ -0,0 +1,11 @@
+class Page
+
+  def initialize(id)
+    @id = id
+  end
+
+  def id
+    @id.to_i
+  end
+
+end
---
+++ spec/models/page_spec.rb
@@ -0,0 +1,18 @@
+require 'page'
+
+describe Page do
+
+  describe '#id' do
+    it 'should provide the given id' do
+      page = Page.new(700)
+      page.id.should == 700
+    end
+
+    it 'should return a Fixnum' do
+      page = Page.new('701')
+      page.id.should == 701
+      page.id.should be_a Fixnum
+    end
+  end
+
+end
