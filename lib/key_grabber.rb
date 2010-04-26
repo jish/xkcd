ea9ac4c58d6c650df48f4d8658a111709ad5d078
Josh Lubaway <josh.lubaway@gmail.com>
2010-04-25 22:11:49 -0700

    Caching the current key for five minutes.

--- lib/key_grabber.rb
+++ lib/key_grabber.rb
@@ -4,9 +4,21 @@ require 'mechanize'
 class KeyGrabber
 
   def self.current_key
-    agent = Mechanize.new
-    page  = agent.get("http://xkcd.com")
-    page.at("h3:nth-child(11)").text.delete('^0-9')
+    if fresh? && @current_key
+      @current_key
+    else
+      @last_read = Time.now
+      agent = Mechanize.new
+      page  = agent.get("http://xkcd.com")
+      @current_key = page.at("h3:nth-child(11)").text.delete('^0-9')
+    end
+  end
+
+  def self.fresh?
+    five_minutes = 60 * 5
+    expiration   = Time.now - five_minutes
+
+    @last_read.to_i > expiration.to_i
   end
 
 end
--- spec/models/key_grabber_spec.rb
+++ spec/models/key_grabber_spec.rb
@@ -1,8 +1,33 @@
 require 'key_grabber'
+require 'mocha'
 
 describe KeyGrabber do
-  it "should grab current key location" do
-    key = KeyGrabber.current_key
-    key.should be_a_kind_of String
+
+  describe '#current_key' do
+    it 'should grab the current key' do
+      key = KeyGrabber.current_key
+      key.should be_a_kind_of String
+    end
+
+    it 'should cache the result' do
+      agent = Mechanize.new
+      Mechanize.expects(:new).once.returns(agent)
+
+      KeyGrabber.current_key
+      KeyGrabber.current_key
+    end
   end
+
+  describe '#fresh' do
+    it 'should be fresh within the last five minutes' do
+      KeyGrabber.instance_variable_set("@last_read", Time.now - 1)
+      KeyGrabber.fresh?.should == true
+    end
+
+    it 'should not be fresh if it is more than five minutes old' do
+      KeyGrabber.instance_variable_set("@last_read", Time.now - 1000)
+      KeyGrabber.fresh?.should == false
+    end
+  end
+
 end
